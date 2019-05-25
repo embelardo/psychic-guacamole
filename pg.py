27d25f51e56a8faaab4d3a84c011a47b051f5e83
Miko Belardo <embelardo@gmail.com>
2019-05-25 16:50:09 -0400

    Create main script with argument parser.

---
+++ pg.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Upload Jira tickets from XML file to Zotero web application.')
+parser.add_argument(
+    'xml-filepath',
+    metavar='xml-filepath',
+    help='filepath of XML file that contains Jira tickets')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
