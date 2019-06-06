0550c0db8272de59488e6a3795d9cda8e64c76f3
Miko Belardo <embelardo@gmail.com>
2019-06-06 05:08:00 -0400

    Implement script.

--- ref_xml.py
+++ ref_xml.py
@@ -8,12 +8,16 @@ import urllib.request
 import xmltodict
 
 
-logging.basicConfig(
-    filename='ref_xml.log',
-    level=logging.DEBUG
-)
 log = logging.getLogger('ref_xml')
 log.setLevel(logging.DEBUG)
+
+log_file_handler = logging.FileHandler('ref_xml.log')
+log_console_handler = logging.StreamHandler()
+log_formatter = logging.Formatter('%(asctime)s [%(name)s] [%(levelname)s] %(message)s')
+log_file_handler.setFormatter(log_formatter)
+log_console_handler.setFormatter(log_formatter)
+log.addHandler(log_file_handler)
+log.addHandler(log_console_handler)
 
 log.debug('Session (start) %s' % ('-' * 80))
 
@@ -34,12 +38,6 @@ with open(args.xml_filepath) as fxml:
     f.write(fxml.read())
     f.close()
 
-print('Title           : ' + unt_obj.rss.channel.item.title.cdata)
-print('Link            : ' + unt_obj.rss.channel.item.link.cdata)
-print('Key             : ' + key)
-print('Type            : ' + unt_obj.rss.channel.item.type.cdata)
-print('Component       : ' + unt_obj.rss.channel.item.component.cdata)
-
 log.debug('Title           : ' + unt_obj.rss.channel.item.title.cdata)
 log.debug('Link            : ' + unt_obj.rss.channel.item.link.cdata)
 log.debug('Key             : ' + key)
@@ -52,7 +50,6 @@ for customfield in unt_obj.rss.channel.item.customfields.customfield:
         affected_clients = []
         for label in customfield.customfieldvalues.label:
             affected_clients.append(label.cdata)
-        print('Affected Clients: {0}'.format(affected_clients))
         log.debug('Affected Clients: {0}'.format(affected_clients))
 
 # RPM Upgrade List
@@ -61,29 +58,19 @@ for customfield in unt_obj.rss.channel.item.customfields.customfield:
         rpm_upgrade_list = []
         for customfieldvalue in customfield.customfieldvalues.customfieldvalue:
             rpm_upgrade_list.append(customfieldvalue.cdata)
-        print('RPM Upgrade List: {0}'.format(rpm_upgrade_list))
         log.debug('RPM Upgrade List: {0}'.format(rpm_upgrade_list))
 
 # Changesets
-print('Comments:')
-log.debug('Comments:')
+log.debug('List of Patches:')
 regex = re.compile(r'.*(https://forge.intelerad.com/hg[^\s"]+)"[\s]+[^\s>]+>([^\s>]+)<[^\s]+[\s]+[^\s]+[\s]+([^\s]+)')
 for comment in unt_obj.rss.channel.item.comments.comment:
     if regex.search(comment.cdata):
-        print('    Comment (start)')
-        log.debug('    Comment (start)')
         for match in regex.finditer(comment.cdata):
             patch_url = match.group(1).replace("rev", "raw-rev")
             patch_file = "{0}_{1}.patch".format(match.group(2).replace(":", "_"), match.group(3))
-            print("        Patch URL (File): {0} ({1})".format(patch_url, patch_file))
-            log.debug("        Patch URL (File): {0} ({1})".format(patch_url, patch_file))
+            log.debug("    Patch URL (File): {0} ({1})".format(patch_url, patch_file))
             # f = open(patch_file, "w")
             # f.write(urllib.request.urlopen(patch_url).read().decode('utf-8'))
             # f.close()
-        print('    Comment (end)')
-        log.debug('    Comment (end)')
-    else:
-        print('    Comment (no patch)')
-        log.debug('    Comment (no patch)')
 
-log.debug('Session (end) %s' % ('-' * 80))
+log.debug('Session (end)   %s' % ('-' * 80))
